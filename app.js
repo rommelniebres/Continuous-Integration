6ca79828031e15033b3e7984e5c6a9e184da3962
rommelniebres <rommelniebresj@gmail.com>
2021-06-03 17:34:21 +0800

    random codes to feature 1

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const WorkingDirectory = require("./models/working-directory");
-const GitCommand = require("./models/git-command");
+const WorkingDirectory = require('./models/working-directory');
+const GitCommand = require('./models/git-command');
 
 //Initialize variables
 let wd = new WorkingDirectory();
-wd.addFile("index.html", "views", "<html>Hello</html>");
-wd.addFile("index.js", "assets/scripts", "alert('Hi!')");
+wd.addFile('index.html', 'views', '<html>Hello</html>');
+wd.addFile('index.js', 'assets/scripts', "alert('Hi!')");
 
 let git = new GitCommand(wd);
-
 
 //1. Initialize git workspace
 git.init();
@@ -16,13 +15,16 @@ git.init();
 console.log(git.status());
 
 //3. Add as index
-console.log(git.add("views/index.html"));
+console.log(git.add('views/index.html'));
 
 //4. Check status
 console.log(git.status());
 
 //5. Commit
-console.log(git.commit("Added index.html file"));
+console.log(git.commit('Added index.html file'));
 
 //6. Push
 console.log(git.push());
+
+//7. random code 1
+console.log('random 1');
