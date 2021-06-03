2afcc41cafbc2a33d7b2bddde500a1a53a9691d2
rommelniebres <rommelniebresj@gmail.com>
2021-06-03 17:28:53 +0800

    first commit

---
+++ models/git-command.js
@@ -0,0 +1,66 @@
+class GitCommand {
+	constructor(working_directory) {
+		this.working_directory = working_directory;
+	}
+	//Command: git init
+	init() {
+		this.staging = [];
+		this.local_repository = [];
+		return 'Initialized as empty Git repository.';
+	}
+
+	//Command: git status
+	status() {
+		/*
+            For assignment #1:
+            Create logic here and run unit testing.
+        */
+
+		let change = Object.keys(this.working_directory.new_changes); //Get the object keys of changes if therese any
+		if (change.length > 0) {
+			change = `You have ${change.length} change/s.\n${change[0]}\n${change[1]}`; //Output a description on how many and what file(s) are changed
+		} else {
+			change = `You have 0 change/s.\n`;
+		}
+		return change;
+	}
+
+	//Command: git add <filename/file directory/wildcard>
+	add(path_file) {
+		let modified_files = this.working_directory.new_changes;
+
+		if (modified_files[path_file]) {
+			this.staging.push(modified_files[path_file]);
+			delete modified_files[path_file];
+		} else {
+			/*
+            For assignment #2:
+            Create logic here and run unit testing.
+            Don't forget to uncomment the unit tests.
+        */
+			return `Failed to add ${path_file}! File is not modified or missing.`;
+		}
+		return 'Successfully added as index file/s.';
+	}
+
+	//Command: git commit -m "<message>"
+	commit(message) {
+		if (this.staging.length > 0) {
+			this.local_repository.push({ message: message, files: this.staging });
+			this.staging = [];
+			return 'Done committing to local repository.';
+		}
+		return 'Nothing to commit.';
+	}
+
+	//Command: git push
+	push() {
+		if (this.local_repository.length > 0) {
+			return 'Done pushing to remote repository.';
+		} else {
+			return 'Nothing to push. No committed file found.';
+		}
+	}
+}
+
+module.exports = GitCommand;
