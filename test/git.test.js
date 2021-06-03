79296e0b5a1d047dd48210abd5c1cb0861f154e6
rommelniebres <rommelniebresj@gmail.com>
2021-06-03 17:43:47 +0800

    added 2 test

--- test/git.test.js
+++ test/git.test.js
@@ -23,6 +23,31 @@ describe('Testing GitCommand.status()', function () {
 
 		expect(output).to.equal('You have 0 change/s.\n');
 	});
+	it('Should success with exact path_file "views/index.html"', function () {
+		let wd = new WorkingDirectory();
+		wd.addFile('index.html', 'views', '<html>Hello</html>');
+
+		let git = new GitCommand(wd);
+		git.init();
+
+		let path_file = 'views/index.html';
+		let output = git.add(path_file);
+
+		expect(output).to.equal('Successfully added as index file/s.');
+	});
+
+	it('Should failed with missing path_file "views/error404.html"', function () {
+		let wd = new WorkingDirectory();
+		wd.addFile('index.html', 'views', '<html>Hello</html>');
+
+		let git = new GitCommand(wd);
+		git.init();
+
+		let path_file = 'views/error404.html';
+		let output = git.add(path_file);
+
+		expect(output).to.equal(`Failed to add ${path_file}! File is not modified or missing.`);
+	});
 });
 
 // describe("Testing GitCommand.add()", function(){
